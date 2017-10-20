1f6f7b80614fde7233ee78c243e8d523f796ad74
Jacob Sharf <jacobsharf@gmail.com>
2017-10-20 00:52:52 -0700

    Incremental incomplete work on kalman filter.

--- filter/kalman.h
+++ filter/kalman.h
@@ -11,24 +11,21 @@ using symbolic::NumericValue;
 
 using Number = double;
 
-template <size_t ROWS, size_t COLS>
-class ParametricMatrix {
-
-};
-
 #template <size_t kNumStates, size_t kNumControls, size_t kNumSensors>
-class Kalman {
+class KalmanFilter {
   public:
-    using StateMatrixType = Matrix<kNumStates, kNumStates, Number>;
-    using ControlMatrixType = Matrix<kNumStates, kNumControls, Number>;
-    using ControlMatrixSymbolicType = Matrix<kNumStates, kNumControls, std::unique_ptr<Expression>>;
+    using StateMatrixType = Matrix<kNumStates, kNumStates, symbolic::Expression>;
+    using ControlMatrixType = Matrix<kNumStates, kNumControls, std::unique_ptr<Expression>>;
     using SensorTransformType = Matrix<kNumSensors, kNumStates, Number>;
     using SensorVectorType = Matrix<kNumStates, 1, Number>;
     using ControlVectorType = Matrix<kNumControls, 1, Number>;
+    KalmanFilter(StateMatrixType state_matrix, ControlMatrixType control_matrix, SensorTransformType sensor_transform)
     // Returns two column vectors. Column 0 contains predicted state averages.
     // Column 1 contains prediction variances for each state parameter.
-    Matrix<kNumStates, 2, Number> predict(double time_s, ControlVectorType controls);
-    Matrix<kNumStates, 2, Number> update(double time_s, SensorVectorType);
+    // Initialize sets initial values for X and P.
+    Matrix<kNumStates, 2, Number> initialize(double time_s, StateVectorType initial_state, StateCovarianceType state_covariance);
+    Matrix<kNumStates, 2, Number> predict(double time_s, ControlVectorType controls, ControlCovarianceMatrix process_noise);
+    Matrix<kNumStates, 2, Number> update(double time_s, SensorVectorType sensors, SensorCovarianceMatrix sensor_covariance);
 };
 
 #endif /* KALMAN_H */
