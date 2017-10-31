b7417e15b20b5e34f3435fd0c8627898775b9f1a
Jacob Sharf <jacobsharf@gmail.com>
2017-10-30 23:00:52 -0700

    Adds symbolic_util (helper symbolic functions).
    The first of which is sigmoid. I wonder what sigmoid is useful for...

---
+++ symbolic/symbolic_util.h
@@ -0,0 +1,15 @@
+#ifndef SYMBOLIC_UTIL_H
+#define SYMBOLIC_UTIL_H 
+
+#include "math/symbolic/expression.h"
+#include "math/symbolic/numeric_value.h"
+
+#include <memory>
+
+namespace symbolic {
+  Expression Sigmoid(Expression a) {
+    return CreateExpression("1") / (CreateExpression("1") + ExponentExpression(NumericValue::e, CreateExpression("-x")));
+  }
+}  // namespace symbolic
+
+#endif /* SYMBOLIC_UTIL_H */
