06b669ebe4b5d39a157d6230ea3262c14aee5963
Jacob Sharf <jacobsharf@gmail.com>
2017-10-31 00:05:29 -0700

    Fix for sigmoid.

--- symbolic/symbolic_util.h
+++ symbolic/symbolic_util.h
@@ -1,5 +1,5 @@
 #ifndef SYMBOLIC_UTIL_H
-#define SYMBOLIC_UTIL_H 
+#define SYMBOLIC_UTIL_H
 
 #include "math/symbolic/expression.h"
 #include "math/symbolic/numeric_value.h"
@@ -7,9 +7,11 @@
 #include <memory>
 
 namespace symbolic {
-  Expression Sigmoid(Expression a) {
-    return CreateExpression("1") / (CreateExpression("1") + ExponentExpression(NumericValue::e, CreateExpression("-x")));
-  }
+Expression Sigmoid(Expression a) {
+  return CreateExpression("1") /
+         (CreateExpression("1") +
+          ExponentExpression(NumericValue::e, CreateExpression("-1 * x")));
+}
 }  // namespace symbolic
 
 #endif /* SYMBOLIC_UTIL_H */
